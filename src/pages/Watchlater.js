472f0894a49bc1ae1878719a28f46a7e21c0a6d6
mahimagupta24 <mahimagupta2401@gmail.com>
2023-07-29 19:49:15 +0530

    add styles

--- src/pages/Watchlater.js
+++ src/pages/Watchlater.js
@@ -1,32 +1,37 @@
-import { useContext, useEffect } from "react"
-import { VideosContext } from "../context/VideosContext"
-import { videos } from "../data"
+import { useContext, useEffect } from "react";
+import { VideosContext } from "../context/VideosContext";
+import { videos } from "../data";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
 
-export default function WatchLater(){
-    const {watchLaterList,setWatchLaterList,handleWatchLater} = useContext(VideosContext)
+export default function WatchLater() {
+  const { watchLaterList, setWatchLaterList, handleWatchLater } =
+    useContext(VideosContext);
 
-    const watchLaterVideos = videos.filter(({_id})=>watchLaterList.includes(_id))
-    // console.log(watchLaterList)
-    console.log(watchLaterVideos)
+  const watchLaterVideos = videos.filter(({ _id }) =>
+    watchLaterList.includes(_id)
+  );
+  // console.log(watchLaterList)
+  console.log(watchLaterVideos);
 
-    useEffect(()=>{
-  const storeWatchedLaterList=localStorage.getItem("watchLaterList")
-  if(storeWatchedLaterList){
-    setWatchLaterList(JSON.parse(storeWatchedLaterList))
-  }
-    },[])
-    return <div>
-       {watchLaterVideos.map(({_id,thumbnail,views,creator})=><div key={_id}>
-        <img src={thumbnail}/>
-        <p>{creator}</p>
-        <p>{views}</p>
-        <div>
-              <WatchLaterIcon
-                className="watchlater"
-                onClick={() => handleWatchLater(_id)}
-              />
-            </div>
-       </div>)}
+  useEffect(() => {
+    const storeWatchedLaterList = localStorage.getItem("watchLaterList");
+    if (storeWatchedLaterList) {
+      setWatchLaterList(JSON.parse(storeWatchedLaterList));
+    }
+  }, []);
+  return (
+    <div className="main-container">
+      {watchLaterVideos.map(({ _id, thumbnail, views, creator }) => (
+        <div key={_id}>
+          <img src={thumbnail} />
+          <p>{creator}</p>
+          <p>{views}</p>
+          <WatchLaterIcon
+            className="watchlater"
+            onClick={() => handleWatchLater(_id)}
+          />
+        </div>
+      ))}
     </div>
+  );
 }
